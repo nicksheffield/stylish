d71f18aa86b2a7f05188e082b7e71d7941d15288
Nick Sheffield <numbereft@gmail.com>
2018-10-12 12:02:02 +1300

    filter out falsy values

--- src/resolve.js
+++ src/resolve.js
@@ -12,12 +12,13 @@ const convert = (str) => str
 		if (styles.get(prop)) return resolve(styles.get(prop))
 		
 		console.warn(`Stylish: Invalid style key "${prop}"`)
-		return {}
+		return null
 	})
 	.reduce((acc, obj) => ({...acc, ...obj}), {})
 
 
 const resolve = (...args) => flatten(args)
+	.filter(item => item)
 	.map(item => typeof item === 'string' ? convert(item) : item)
 	.reduce((acc, obj) => ({...acc, ...obj}), {})
 
--- test.js
+++ test.js
@@ -5,7 +5,7 @@ stylish.addColor('primary', '#DD0000')
 stylish.addStyle('my-style', 'bb-w:1 bb-c:#eee my-style2')
 stylish.addStyle('my-style2', 'txt:red bg:primary')
 
-const i2 = stylish.resolve('my-style')
+const i2 = stylish.resolve('')
 const o2 = {
 	borderBottomWidth: 1,
 	borderBottomColor: '#eee',
